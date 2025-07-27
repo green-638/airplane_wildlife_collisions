47e2102d96e798e93d82b98f1bcb0c857c40ff8a
Connor Hall <connor582h@gmail.com>
2025-07-27 15:43:22 -0400

    updated docstring and comments

--- annual_collision_count.py
+++ annual_collision_count.py
@@ -6,27 +6,30 @@ def annual_collision_count(df, arpts, start_year=1990, end_year=2024):
     
     Args:
         df (dataframe): the main dataframe
-        arpts (list of str): list of airports
+        arpts (list of str): airports to select from dataframe
         start_year (int): starting year of timeframe
         end_year (int): ending year of timeframe
         
     Returns:
         pandas dataframe: contains data for species, year, incident count
     '''
-    # main dataframe
-    dfs = []
+    # list of dataframes
+    dataframes = []
 
     for year in range(start_year, end_year+1):
+        # select rows with matching airport and year
         current_year = df[(df['AIRPORT'].isin(arpts)) &
                             (df['INCIDENT_YEAR'] == year)]
+        
+        # annual count
         total_count = len(current_year)
             
-        # insert current year's data into dataframe
+        # create dataframe for current year's data
         current_data = pd.DataFrame({'Year': [year],
                                 'Total Count': [total_count]})
         
-        # concat data to main dataframe
-        dfs.append(current_data)
-   
-    combined_df = pd.concat(dfs, ignore_index=True)
+        dataframes.append(current_data)
+        
+    # combine all annual counts into single dataframe
+    combined_df = pd.concat(dataframes, ignore_index=True)
     return combined_df
